c00e9a20340f3e112667bded7762ff252df6730c
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-03-07 01:44:33 +0400

    Son

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-var ImageList = ["img1.avif", "img2.avif", "img3.avif", "img4.avif", "img6.avif", "img7.avif", "img8.avif", "img9.jpg"];
-const array_Count = 4;
+var ImageList = ["img1.avif", "img2.avif", "img3.avif", "img4.avif", "img5.avif", "img6.avif"];
+const row = 4;
+const columun = 3;
 var Click_Items = [];
-//' style="background-image:url(images/${ImageList[number]});"'
+var boolen = false;
 var IndexList = [];
 function main() {
     let mytable = document.getElementById("myTable");
@@ -10,9 +11,9 @@ function main() {
 
     //alert(ImageList.length/2)
     let num = 0;
-    for (let i = 0; i < array_Count; i++) {
+    for (let i = 0; i < columun; i++) {
         content += "<tr>";
-        for (let k = 0; k < array_Count; k++) {
+        for (let k = 0; k < row; k++) {
 
             let id = `${num}`;
             num++;
@@ -29,11 +30,11 @@ function main() {
 }
 
 function click1(event) {
+    if (boolen) {
+        return;
+    }
     let value = event.target;
-
-
     Click_Items.push(value);
-
     value.classList.remove("kapali");
     value.classList.remove("kapali2");
     //alert(value.classList);
@@ -42,16 +43,32 @@ function click1(event) {
     Click_Index_Check();
 
 }
-
+var WinCount =columun*row;
 function Click_Index_Check() {
     if (Click_Items.length > 1) {
+        boolen = true;
         if (Click_Items[0].style.backgroundImage == Click_Items[1].style.backgroundImage) {
-            var audio = new Audio('sounds/win.mp3');
+            let audio = new Audio('sounds/win.mp3');
             audio.play();
             setTimeout(() => {
-                Click_Items[0].classList.add("animate__hinge");
-                Click_Items[1].classList.add("animate__hinge");
+                // Click_Items[0].classList.add("animate__hinge");
+                // Click_Items[1].classList.add("animate__hinge");
+                // alert(IndexList)
+                Click_Items.forEach(element => {
+                    element.classList.add("animate__hinge");
+                    IndexList.splice(element.id, 1);
+                    WinCount--;
+                });
+             
+                if (WinCount <= 0) {
+
+                    var audio = new Audio('sounds/shor.mp3');
+                    audio.play()
+                    return;
+                }
                 Click_Items = [];
+                boolen = false;
+
             }, 1000);
 
         }
@@ -70,13 +87,14 @@ function Click_Index_Check() {
                 }, 1090);
                 element.addEventListener("click", click1);
                 setTimeout(() => {
-
                     element.classList.remove("animate__flipInY");
+                    boolen = false;
                 }, 1500);
             });
             Click_Items = [];
             // alert("false");
         }
+
     }
 
 }
@@ -104,10 +122,10 @@ function tumunuKapat() {
 function main2() {
     let randomnum = null;
     let Url_Value = ImageList[0];
-    for (let index1 = 0; index1 < 8; index1++) {
+    for (let index1 = 0; index1 < (row * columun) / 2; index1++) {
         Url_Value = ImageList[index1]
         for (let l = 0; l < 2; l++) {
-            randomnum = getRandom(16)
+            randomnum = getRandom(row * columun);
             let item = document.getElementById(randomnum);
             IndexList.push(randomnum);
             item.style.backgroundImage = `url('images/${Url_Value}')`
@@ -120,15 +138,15 @@ function main2() {
 function getRandom(max) {
 
     while (true) {
-        let boolen = true;
+        let isok = true;
         let n = Math.floor(Math.random() * max);
         for (let index = 0; index < IndexList.length; index++) {
 
             if (IndexList[index] == n) {
-                boolen = false;
+                isok = false;
             }
         }
-        if (boolen) {
+        if (isok) {
             return n;
         }
 
